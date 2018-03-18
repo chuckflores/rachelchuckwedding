165ce90356e179d0a4f732f475810996017ab0bd
chuckflores <chuck.e.flores@gmail.com>
2018-03-18 18:27:56 -0400

    fix deployment

--- config.rb
+++ config.rb
@@ -48,10 +48,7 @@ page '/*.txt', layout: false
 activate :livereload
 
 activate :deploy do |deploy|
-  deploy.method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.deploy_method = :git
+  deploy.branch = 'master'
+  deploy.build_before = true
 end
