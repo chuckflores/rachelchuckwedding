7954a7974ef795b74051b71cebb74b58a091c3ef
chuckflores <chuck.e.flores@gmail.com>
2018-05-02 00:48:29 -0400

    Automated commit at 2018-05-02 04:48:28 UTC by middleman-deploy 2.0.0-alpha

--- js/site.js
+++ js/site.js
@@ -1,1 +1,70 @@
-function closeNav(){document.getElementById("myNav").style.display="none",document.getElementById("myNav").style.opacity="0"}function openNav(){document.getElementById("myNav").style.display="block",document.getElementById("myNav").style.opacity="1"}function onScroll(){var o=$(document).scrollTop();$("nav a").each(function(){var t=$(this),e=$(t.attr("href"));e.position().top<=o&&e.position().top+e.height()>o?($("nav--desktop-link").removeClass("active"),t.addClass("active")):t.removeClass("active")})}$(document).ready(function(){$(document).on("scroll",onScroll),$('a[href^="#"]').on("click",function(o){o.preventDefault(),$(document).off("scroll"),$("a").each(function(){$(this).removeClass("active")}),$(this).addClass("active");var t=this.hash,e=$(t);$("html, body").stop().animate({scrollTop:e.offset().top},600,"swing",function(){window.location.hash=t,$(document).on("scroll",onScroll)})})});
+// Responsive nav
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+    document.getElementById("myNav").style.opacity = "0";
+}
+
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+    document.getElementById("myNav").style.opacity = "1";
+}
+
+// Lightbox
+$(document).ready(function(){
+  $('.venobox').venobox({
+    closeBackground: "#FFFFFF",
+    closeColor: "#FF00FF",
+    overlayColor: 'rgba(255,255,255,0.85)',
+    arrowsColor: "#EA1DEA",
+    titleattr: 'data-title',
+    titleBackground: "rgba(255,255,255,1)",
+    titleColor: "#19191A"
+  });
+});
+
+// Smooth Scroll
+$(document).ready(function() {
+  $(document).on("scroll", onScroll);
+
+	$('a[href^="#"]').on('click', function (e) {
+	    e.preventDefault();
+      $(document).off("scroll");
+
+			$('a').each(function () {
+				$(this).removeClass('active');
+			})
+			$(this).addClass('active');
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 600, 'swing', function () {
+	        window.location.hash = target;
+          $(document).on("scroll", onScroll);
+	    });
+	});
+});
+
+// Change Nav Link Active Class on Scroll Desktop
+function onScroll(event){
+		var scrollPosition = $(document).scrollTop();
+
+		$('nav a').each(function () {
+			var currentLink = $(this);
+
+			var refElement = $(currentLink.attr("href"));
+
+			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+				$('nav--desktop-link').removeClass('active');
+				currentLink.addClass('active');
+			}
+			else{
+				currentLink.removeClass('active');
+			}
+		});
+	}
